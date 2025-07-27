25d41867117da2ede1d7cc143ba8b01100402084
Cody Wellman <cwellman@quartz.systems>
2025-07-27 07:09:03 -0400

    Create fan-control.sh

---
+++ fan-control.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd /opt/dellemc/diag/bin
+
+./fantool --set --fan=1 --speed=25
+./fantool --set --fan=2 --speed=25
+./fantool --set --fan=3 --speed=25
+./fantool --set --fan=4 --speed=25
