3062c918e4edc570efd14d84d777934f036b190b
Cody Wellman <cody@codexmicro.systems>
2025-09-02 19:06:30 -0400

    Working script

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 
 ```
 curl -O https://raw.githubusercontent.com/quartzsystems/dellemc-edge3400-fan-control/refs/heads/main/install.sh
+chmod u+x install.sh
 ./install.sh
 ```
--- install.sh
+++ install.sh
@@ -11,8 +11,10 @@ dpkg -i dn-diags-vep4600.deb
 
 # Downloads and installs the systemd service.
 curl -o /etc/systemd/system/fan-control.service https://raw.githubusercontent.com/quartzsystems/dellemc-edge3400-fan-control/refs/heads/main/fan-control.service
+
 # Downloads the script into /root/.
 curl -o /root/fan-control.sh https://raw.githubusercontent.com/quartzsystems/dellemc-edge3400-fan-control/refs/heads/main/fan-control.sh
+chmod u+x fan-control.sh
 
 # Reloads and enables new systemd service.
 systemctl daemon-reload
