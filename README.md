6467672f8846d64687085152c7c57a5072500d30
Cody Wellman <cody@codexmicro.systems>
2025-09-02 19:01:30 -0400

    Install script

---
+++ README.md
@@ -0,0 +1,6 @@
+## Installation
+
+```
+curl -O https://raw.githubusercontent.com/quartzsystems/dellemc-edge3400-fan-control/refs/heads/main/install.sh
+./install.sh
+```
---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Installs Dell EMC VEP4600 diagnostics package dependencies.
+apt install ipmitool coreboot-utils -y
+
+# Downloads diagnostics package.
+curl -O https://raw.githubusercontent.com/quartzsystems/dellemc-edge3400-fan-control/refs/heads/main/dn-diags-vep4600.deb
+
+# Installs the diagnostics package.
+dpkg -i dn-diags-vep4600.deb
+
+# Downloads and installs the systemd service.
+curl -O /etc/systemd/system/fan-control.service https://raw.githubusercontent.com/quartzsystems/dellemc-edge3400-fan-control/refs/heads/main/fan-control.service
+
+# Downloads the script into /root/.
+curl -O /root/fan-control.sh https://raw.githubusercontent.com/quartzsystems/dellemc-edge3400-fan-control/refs/heads/main/fan-control.sh
+
+# Reloads and enables new systemd service.
+systemctl daemon-reload
+systemctl enable fan-control.service
